a083ee3eaf0ae99d1e8cf845acc62a9a123580cf
Eduardo Viana Pessoa <eduardo.vianap@gmail.com>
2019-03-27 12:00:26 -0300

    Database connection, queries, authentication and first get request! ;)

--- src/main.py
+++ src/main.py
@@ -1,44 +1,79 @@
 # !/usr/bin/python
 import psycopg2
-from src import config
+import json
+from configparser import ConfigParser
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_httpauth import HTTPBasicAuth
+from flask_restful import Api
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 api = Api(app)
 
 
+@auth.verify_password
+def verify_password(username, password):
+    if username == 'apitimestorage' and password == '_@P1t1m3$t0r4g3##/':
+        return True
+    else:
+        return False
+
+
+@app.route('/')
+def index():
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('SELECT version()')
+    db_version = cur.fetchone()
+    close(conn)
+    return '<h3>API TimeStorage Running!</h3><h4>PostgreSQL DB Version: ' + str(db_version) + '</h4>'
+
+
+@app.route('/pessoa')
+@auth.login_required
+def pessoa():
+    query = "SELECT * FROM v_pessoa"
+    result = query_db(query, False)
+    return json.dumps(result)
+
+
+def query_db(query, one=False):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(query)
+    r = [dict((cur.description[i][0], value) \
+              for i, value in enumerate(row)) for row in cur.fetchall()]
+    cur.connection.close()
+    return (r[0] if r else None) if one else r
+
+
+def config(filename='util/database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
 def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
     try:
-        # read connection parameters
-        params = config.config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
+        params = config()
         conn = psycopg2.connect(**params)
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
-        cur.close()
+        return conn
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+
+
+def close(conn):
+    if conn is not None:
+        conn.close()
 
 
 if __name__ == '__main__':
-    connect()
+    app.run()
