790e7f63f98ef8c8f4d93d24e7e720c869301470
Eduardo Viana Pessoa <eduardo.vianap@gmail.com>
2019-06-12 14:42:48 -0300

    Registering user.

--- src/main.py
+++ src/main.py
@@ -42,6 +42,7 @@ def cadastrar():
     if not request.json:
         return 'Os dados do JSON não podem estar vazios!', 400
     data = request.get_json()
+    print(data)
     conn = connect()
     cur = conn.cursor()
     query = "SELECT fn_register_user('" + data['nome_pessoa'] + "','" + data['sobrenome_pessoa'] + "','" + \
